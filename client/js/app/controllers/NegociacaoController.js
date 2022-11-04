7ad5f0064b8c55c2803d04e312be713ed11d56ba
Rico <grico484@gmail.com>
2022-11-03 22:53:03 -0400

    feat: Created the DateHelper class

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -10,16 +10,14 @@ class NegociacaoController {
 
     adiciona(event) {
         event.preventDefault();
-        
-        let data = new Date(
-            this._inputData.value
-                .split('-')
-        );
+
         let negociacao = new Negociacao(
-            data,
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
+        
         console.log(negociacao);
+        console.log(DateHelper.dataParaTexto(negociacao.data));
     }
 }
