bdf616ee539e984f314feee194069a042373eb35
Graciliano-Barroso <grico484@gmail.com>
2023-05-11 05:53:42 -0400

    Realizando o parse da resposta

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -24,13 +24,18 @@ class NegociacaoController {
     importaNegociacoes() {
 
         let xhr = new XMLHttpRequest();
-        xhr.open('GET', 'negociacoes/semana');
+        xhr.open('GET', 'negociacoes/xsemana');
         xhr.onreadystatechange = () => {
             if(xhr.readyState == 4) {
                 if(xhr.status == 200) {
                     console.log('Obtendo as negociações');
+                    JSON.parse(xhr.responseText)
+                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
+                        .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                        this._mensagem.texto = 'Negociações importadas com sucesso';
                 } else {
-                    console.log('Não foi posivel conseguir as negociações');
+                    console.log(xhr.responseText);
+                    this._mensagem.texto = 'Não foi possivel obter as negociações'
                 }
             }
         };
